81d9cd63359aa3257f65d6f75d4a0035fe69817e
gepopp <office@sewemo.eu>
2020-04-06 22:34:08 +0200

    first controllers

---
+++ app/Http/Controllers/UserInviteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserInvite;
+use Illuminate\Http\Request;
+
+class UserInviteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserInvite  $userInvite
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserInvite $userInvite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserInvite  $userInvite
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserInvite $userInvite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserInvite  $userInvite
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserInvite $userInvite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserInvite  $userInvite
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserInvite $userInvite)
+    {
+        //
+    }
+}
---
+++ database/factories/UserInviteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UserInvite;
+use Faker\Generator as Faker;
+
+$factory->define(UserInvite::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
